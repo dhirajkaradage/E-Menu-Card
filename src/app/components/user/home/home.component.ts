61562fe41406e363df7bc0fae0dad10442c224d5
Dhiraj Karadage <dhiraj.karadage@amelioratesolutions.com>
2024-01-21 22:28:31 +0530

    user side navbar product list and footer done - admin side listing pages structure and add pages structure done with routing

--- src/app/components/user/home/home.component.ts
+++ src/app/components/user/home/home.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+  productList: Array<any> = [
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+    {
+      url: "https://i.imgur.com/xdbHo4E.png",
+      name: "asg",
+      price: 140,
+    },
+  ];
+  constructor() {}
 }
